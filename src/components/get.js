7582248da089723959f0dfbe451a9bee4a2fa894
ShaneEverittM <shaneemurphy146@gmail.com>
2020-05-18 01:50:21 -0700

    Comments

--- src/components/get.js
+++ src/components/get.js
@@ -5,14 +5,19 @@ const hostname = "http://localhost:8000"
 const Get = ({ name }) => {
     const [text, setText] = useState("");
 
+    //runs at the end of the function iff name changes after the first time
     useEffect(() => {
         const fetchText = async () => {
             const res = await axios.get(`${hostname}/hello/${name}`)
             console.log(res)
-            setText(res.data["message"])
+            setText(res.data["message"]) //triggers a re-render, with one value for text, renders twice
         };
         fetchText();
-    }, []);
+    }, [name]);
+
+    if (text.length === 0) {
+        return <div />
+    }
     return (
         <div>
             {text}
