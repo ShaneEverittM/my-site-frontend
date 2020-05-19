91517ace45a9820fed2a57fc55f3661caf9ee14d
ShaneEverittM <shaneemurphy146@gmail.com>
2020-05-19 15:49:58 -0700

    More _flexible_

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaFolder } from 'react-icons/fa'
+import { FaFolder, FaLinux } from 'react-icons/fa'
 import '../styles/Sidebar.css'
-import { FaRProject } from 'react-icons/fa'
 const SideBar = () => {
     return (
         <div className='sidebar'>
             <header className="sidebar-header">Projects</header>
             <div className='sidebar-list'>
                 <Link to="/filesystem" style={{ all: "unset" }}>
-                    <div className='sidebar-list-item'><FaFolder /> FileSystem</div>
+                    <div className='sidebar-list-item'>
+                        <FaFolder />
+                        FileSystem
+                    </div>
                 </Link>
                 <Link to="/filesystem" style={{ all: "unset" }}>
-                    <div className='sidebar-list-item'><FaFolder /> TBD</div>
-                </Link>
-                <Link to="/filesystem" style={{ all: "unset" }}>
-                    <div className='sidebar-list-item'><FaFolder /> TBD</div>
-                </Link>
-                <Link to="/filesystem" style={{ all: "unset" }}>
-                    <div className='sidebar-list-item'><FaFolder /> TBD</div>
+                    <div className='sidebar-list-item'>
+                        <FaLinux />
+                        Scheduler
+                    </div>
                 </Link>
             </div>
         </div>
